97d7e4495362778464606a485f3b36cf2ed8f25c
VSeninecs <60810840+VSeninecs@users.noreply.github.com>
2021-09-21 20:46:12 +0300

    Pievienoti kontakti, un flask

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Sveiki")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Sveiki!!!"
+
+@app.route("/kontakti")
+def kontakti():
+    return "<html><h1>Kas te notiek<h1><html>"
+
+if __name__ == "__main__":
+    app.run(port=80, debug=True)
